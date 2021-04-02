1acc9d6fcf2ec08d48bcda9acc2d1ab292cd58a5
lokesh Goyal <lokeshg421@gmail.com>
2021-04-02 15:49:00 +0530

    optimized

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const {getGeoCode, getForeCast} = require('./utils/utilities')
 
 
+
 //Express Initiated
 const app = express()
 const port = process.env.PORT || 3000
